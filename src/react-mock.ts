80eff3e39692e884637046c1569b6a10eda9d641
Marcellin Nshimiyimana <marcellin.nshimiyimana@ibm.com@Marcellins-MacBook-Pro.local>
2018-01-30 15:02:12 -0500

    feat($initialization): add skeleton for minimum requirements
    add uuid and faker, define the Server class, use javascript generator to generate ids of fake
    objects
    
    BREAKING CHANGE: none

--- src/react-mock.ts
+++ src/react-mock.ts
@@ -1,4 +1,55 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
 // import "core-js/fn/array.find"
 // ...
-export default class DummyClass {}
+import * as uuidV4 from 'uuid/v4'
+import * as faker from 'faker'
+
+// START helper functions
+export interface IDataGenerator {
+  next<T>(
+    count: number,
+    schema: T,
+    returnObject?: boolean
+  ): Array<T> | { [id: string]: T }
+}
+class DataGenerator implements IDataGenerator {
+  nextObject<T>(count: number, schema: T): { [id: string]: T } {
+    return {}
+  }
+  nextArray<T>(count: number, schema: T): Array<T> {
+    return []
+  }
+  next<T>(
+    count: number,
+    schema: T,
+    returnObject = false
+  ): Array<T> | { [id: string]: T } {
+    if (returnObject) {
+      return this.nextObject<T>(count, schema)
+    }
+    return this.nextArray<T>(count, schema)
+  }
+}
+function* Uid() {
+  while (true) {
+    yield uuidV4()
+  }
+}
+// END helper functions
+
+export class Server {
+  on(): Promise<null | Error> {
+    return Promise.reject(new Error('NOT Implemented'))
+  }
+
+  mockGet(
+    endpoint: string,
+    handler: (req: Object, generator: IDataGenerator) => void
+  ): void {
+    throw new Error('NOT Implemented')
+  }
+}
+
+export const Faker = faker
+export const uid = Uid()
+export default new Server()
--- test/react-mock.test.ts
+++ test/react-mock.test.ts
@@ -1,4 +1,4 @@
-import DummyClass from '../src/react-mock'
+import Server, { Server as ServerClass } from '../src/react-mock'
 
 /**
  * Dummy test
@@ -9,6 +9,6 @@ describe('Dummy test', () => {
   })
 
   it('DummyClass is instantiable', () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+    expect(Server).toBeInstanceOf(ServerClass)
   })
 })
