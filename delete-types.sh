c2b5e2541e7e61aa64c4b88807209515fa19a32f
Marcellin Nshimiyimana <marcellin2@gmail.com>
2019-02-12 23:29:44 -0500

    remove types introduced by capacitor

---
+++ delete-types.sh
@@ -0,0 +1,5 @@
+rm -rf ./node_modules/@types/mocha
+rm -rf ./node_modules/@types/sinon
+rm -rf ./node_modules/@types/sinon-chai
+rm -rf ./node_modules/@types/chai
+rm -rf ./node_modules/@types/chai-jquery
